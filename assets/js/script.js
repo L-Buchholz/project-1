4d18792186117157e95d0d7012410709a9b8d7d6
Lauren Buchholz <l.buchholz1988@gmail.com>
2021-11-18 14:37:33 -0700

    Updated calendar JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,16 @@ var day = today.day();
 
 //FUNCTIONS
 
+/*Body header: Sets dates for the current week AND current date, 
+both incorporating days of the week*/
+/*MomentJs: day(1) = Monday, day(7) = following Sunday (based on
+  Sunday (index 0) -- Saturday (index 6) week*/
+var weekStart = moment().day(1).format("MMM Do");
+var weekEnd = moment().day(7).format("MMM Do");
+$("#currentWeek").text(
+  "Week of Monday, " + weekStart + ", " + " to Sunday, " + weekEnd
+);
+
 var holidays = function () {
   var requestUrl =
     "https://calendarific.com/api/v2/holidays?&api_key=430d5f859bcdbc7032378fb6997905cdc22686c1&country=US&year=2021&type=national";
@@ -21,32 +31,43 @@ var holidays = function () {
     })
     /*Returns collected data (if response is received) as an object*/
     .then(function (data) {
-      console.log(data);
-
-      let date = data.response.holidays[0].date.iso;
-      console.log(date);
-
-      /*Determines if current calendar week includes any holidays, by day*/
-      for (var i = 0; i < data.response.holidays.length; i++) {
-        var today1 = moment().format("YYYY-MM-DD");
-        /*Refers to each individual line (day)*/
-        var textArea = $(".schedule").find("textarea");
-
-        if (today1 === date) {
-          /*To test for holidays: Replace "today1" with a holiday in 
-          YYYY-MM-DD format; i.e., "if ("2021-01-01" ==== date)" returns 
-          "Public holiday" on all lines*/
-          $(textArea).val("Public holiday");
+      /*Establishes calendar week as a Monday -- Sunday loop*/
+      for (var calendarDay = 1; calendarDay < 8; calendarDay++) {
+        var currentDay = $("#" + calendarDay);
+        if (day == calendarDay) {
+          currentDay.addClass("present");
+        } else if (day > calendarDay) {
+          currentDay.addClass("past");
         } else {
-          /*Tests code in dev -- not visible on page*/
-          console.log("Not a holiday");
+          currentDay.addClass("future");
+        }
+        /*Saves user-generated text to calendar text areas 
+        (here calendarDay points at the value stored by dayId, 
+        which is defined below)*/
+        var textArea = $(currentDay).find("textarea");
+        var savedValue = localStorage.getItem(calendarDay);
+        $(textArea).val(savedValue);
+
+        /*Determines if current calendar week includes any holidays, by day*/
+        for (var i = 0; i < data.response.holidays.length; i++) {
+          var date = data.response.holidays[i].date.iso;
+          var dayOfWeek = moment().day(calendarDay).format("YYYY-MM-DD");
+          /*Refers to each individual line (calendarDay)
+          To test this on non-holiday weeks: Change === to !== */
+          if (dayOfWeek === date && calendarDay !== day) {
+            $(currentDay).find("textarea").attr("placeholder", "US holiday");
+            $(currentDay).addClass("holiday");
+          } else {
+            /*Tests code in dev -- not visible on page (16 national holidays x 7 days)*/
+            console.log("Not a holiday");
+          }
         }
       }
     });
 };
 holidays();
 
-var emailVarification = function () {
+var emailVerification = function () {
   var email = "ohall1223@gmail.com";
   // document.querySelector("#sign-up-btn")
   var pizzaAPI = `https://www.validator.pizza/email/${email}`;
@@ -67,35 +88,7 @@ var emailVarification = function () {
       }
     });
 };
-emailVarification();
-
-/*Body header: Sets dates for the current week AND current date, 
-both incorporating days of the week*/
-/*MomentJs: day(1) = Monday, day(7) = following Sunday (based on
-  Sunday (index 0) -- Saturday (index 6) week*/
-var weekStart = moment().day(1).format("MMM Do");
-var weekEnd = moment().day(7).format("MMM Do");
-$("#currentWeek").text(
-  "Week of Monday, " + weekStart + ", " + " to Sunday, " + weekEnd
-);
-
-/*Establishes calendar week as a Monday -- Sunday loop*/
-for (var calendarDay = 1; calendarDay < 8; calendarDay++) {
-  var currentDay = $("#" + calendarDay);
-  if (day == calendarDay) {
-    currentDay.addClass("present");
-  } else if (day > calendarDay) {
-    currentDay.addClass("past");
-  } else {
-    currentDay.addClass("future");
-  }
-  /*Saves user-generated text to calendar text areas 
-  (here calendarDay points at the value stored by dayId, 
-  which is defined below)*/
-  var textArea = $(currentDay).find("textarea");
-  var savedValue = localStorage.getItem(calendarDay);
-  $(textArea).val(savedValue);
-}
+emailVerification();
 
 //EVENT LISTENERS
 /*Enable "save" buttons to save items to localStorage)*/
